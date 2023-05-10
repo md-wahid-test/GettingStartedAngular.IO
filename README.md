35b65d0c0c4fff552b0dc9ae21a2804f67d2bbf6
md-wahid-test <132974485+md-wahid-test@users.noreply.github.com>
2023-05-10 11:50:49 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GettingStartedAngular.IO
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/uendi9-mhzbcg)
---
+++ src/app/products.ts
@@ -0,0 +1,39 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XXL',
+    price: 1000,
+    description: 'An extra large phone with one of the best outfits',
+  },
+  {
+    id: 2,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 3,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 4,
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
